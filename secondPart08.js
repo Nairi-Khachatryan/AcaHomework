890b8b1037bd8d7b51136a4a020014aef433f5f6
Nairi Khachatryan <nairikhachatryan@Nairis-MacBook-Air.local>
2024-07-05 17:50:11 +0400

    Дополнения в домашней работе

---
+++ secondPart08.js
@@ -0,0 +1,19 @@
+// Task 2
+// What will be logged and explain why
+
+let a = 5;
+let b = 10;
+let c = 15;
+
+const result = a < b && b < c ? '1' : a > b && b > c ? '2' : '3'
+
+// patasxan
+//   result equal '1' and '3'
+// araji ternar@ kta true ev kunenanq '1' isk erkrord ternar@ kta false ev kunenanq '3' 
+// menak chgitem petqa result - i mech miasin tpvi te probelov 
+
+console.log(result);
+
+
+// )))) 
+// naeci patasxan@ pastoren qani vor ternar@  true unecav hench et el tpech u arten false i mech grvach@ bac toxech
---
+++ secondPart11.js
@@ -0,0 +1,19 @@
+//Task 5 
+//What will be logged and explain why 
+ 
+
+ const num = 30
+ const boolean = false
+ const result = num < 18 ? '1' : boolean && num >= 18 && num <= 65 ? '2' : '3'
+
+ //patasxan
+ //petq e vor tpvi resulti mech '3' qani vor skzbic ternar@ stuguma num < 18 stanum enq false 
+ // ev mtnum enq ira 2 mas@ vortex stugum enq boolean && num  >= 18  et masum stanum enq false qani vor ev@ic araj exach false arjeqna verchnum heto norica hamematvum evov false && num <= 65 u eli stanum enq false qani vor && et arjeq@ verchrech u tpum '3'
+ console.log(result);
+ 
+ //ayo es chisht ei)))
+ 
+ 
+ 
+ 
+ 
